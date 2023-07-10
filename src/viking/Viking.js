c289b32703a61ebdf2a535410bcf6f777b7dc824
cesarconte <yugurta@gmail.com>
2023-07-10 19:03:27 +0200

    Viking.test.js done & ready

--- src/viking/Viking.js
+++ src/viking/Viking.js
@@ -1,3 +1,22 @@
-class Viking {}
+const Soldier = require("../soldier/Soldier.js");
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 module.exports = Viking;
