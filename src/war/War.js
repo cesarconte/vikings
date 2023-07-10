2606551799e9e76d1764a1f957c6f7b421cef4da
cesarconte <yugurta@gmail.com>
2023-07-10 19:28:10 +0200

    fixed showStatus 3rd test

--- src/war/War.js
+++ src/war/War.js
@@ -50,12 +50,8 @@ class War {
   showStatus() {
     if (this.saxonArmy.length === 0) {
       return "Vikings have won the war of the century!";
-    } else if (this.vikingArmy.length === 0) {
-      return "Saxons have fought for their lives and survived another day...";
-    } else {
-      return "Vikings and Saxons are still in the thick of battle.";
-    }
   }
+}
 }
 
 module.exports = War;
